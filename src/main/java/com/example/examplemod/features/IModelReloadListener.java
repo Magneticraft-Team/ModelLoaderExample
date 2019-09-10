afe766382597768a873b158fe5a32b2dd9d65949
cout970 <thecout970@gmail.com>
2019-09-10 21:03:16 +0200

    Clean branch for the 1.14 version

--- src/main/java/com/example/examplemod/features/IModelReloadListener.java
+++
@@ -1,6 +0,0 @@
-package com.example.examplemod.features;
-
-public interface IModelReloadListener {
-
-    void reloadModels();
-}
--- src/main/java/com/example/examplemod/features/animated_block/AnimatedTile.java
+++
@@ -1,5 +0,0 @@
-package com.example.examplemod.features.animated_block;
-
-import net.minecraft.tileentity.TileEntity;
-
-public class AnimatedTile extends TileEntity {}
